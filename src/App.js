83798b2051e718df72b1eed1bb42f64b68e72775
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-01-14 19:55:28 +0530

    Implemented redux in react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import store from './redux/store';
+import { Provider } from 'react-redux' 
+
+function App() {
+  return (
+
+    <Provider store={store} > 
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
+    
+  );
+}
+
+export default App;
---
+++ src/components/CakeContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux/cake/cakeActions'
+
+function CakeContainer(props) {
+  return (
+    <div>
+      
+      <h2> Number of Cakes - {props.numOfCakes} </h2>
+      <button onClick={props.buyCake} > Add more cakes </button>
+
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    numOfCakes: state.numOfCakes
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return{
+    buyCake: () => dispatch(buyCake())
+  }
+}
+
+export default connect( 
+  mapStateToProps, 
+  mapDispatchToProps )(CakeContainer)
+  
